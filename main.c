eb65812b0b1d7d046b15c7d7330923cf84ba7602
Diego Miranda Neves Gomes <diego-mng@outlook.com>
2015-07-27 14:24:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "scanner_declarations.h"
+#include "errors.h"
 #include "scanner.h"
+#include "symbol_table.h"
 
 int main()
 {
-	initializer_scanner(fopen("arquivo_cod_portugol.txt", "r"));
-	return 0;
+	initializer_scanner(fopen("cod_portugol.txt", "r"));
+	entry_t *t;
+	t = initialize_table(0);
 }
