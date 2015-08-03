f21330b5588b5a83a2857517ae6cc3f577b4fc24
Diego Miranda Neves Gomes <diego-mng@outlook.com>
2015-08-03 15:03:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "errors.h"
 #include "scanner.h"
+#include "errors.h"
 #include "symbol_table.h"
+#include "parser.h"
 
 int main()
 {
-	initializer_scanner(fopen("cod_portugol.txt", "r"));
 	entry_t *t;
 	t = initialize_table(0);
+	initialize_parser(fopen("cod_portugol.txt", "r"), t);
+	parse();
 }
