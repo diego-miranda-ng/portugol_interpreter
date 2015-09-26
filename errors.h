52dcc8a58b8a30695c788b0f61246d74e2ff3e3a
Diego Miranda Neves Gomes <diego-mng@outlook.com>
2015-09-26 20:32:26 -0300

    Ajustes na biblioteca.
    Este arquivo agora contém apenas o cabeçalho de algumas funções e as principais declarações, sendo assim foi criado um arquivo .c que contém o conteúdo das funções declaradas aqui.

--- errors.h
+++ errors.h
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#include "scanner.h"
 
+// Aqui são declaradas as constantes que representam cada tipo que pode ser encontrado
+// durante a análise de um código-fonte.
+/*-----------------------------------------------------------------------------------------*/
 typedef enum _error {
 	error_log,
 	error_info,
@@ -12,92 +13,13 @@ typedef enum _error {
 	error_unknown
 } error_t;
 
-#define errors_tolerance 50
+// Esta função é usada para exibir mensagens de erro durante a análise de um código-fonte.
+// error_t error recebe quall tipo de erro que foi econtrado.
+// char message recebe a mensagem que será exiida.
+/*-----------------------------------------------------------------------------------------*/
+void mark(error_t error, char message[],...);
 
-unsigned int errors_count = 0;
-
-void mark_at(error_t error, position_t position, char message[],...)
-{
-	if(error > error_warning)
-		errors_count++;
-	if(errors_count > errors_tolerance)
-	{
-		printf("%d erros foram encontrados. O processo de análise foi cancelado.", errors_tolerance);
-		exit(EXIT_FAILURE);
-	}
-	switch(error)
-	{
-		case error_log:
-			printf("Log");
-			break;
-		case error_info:
-			printf("Info");
-			break;
-		case error_tip:
-			printf("Tip");
-			break;
-		case error_warning:
-			printf("Atenção em ");
-			break;
-		case error_parser:
-			printf("Erro em ");
-			break;
-		case error_fatal:
-			printf("Erro fatal em ");
-			break;
-		default:
-			printf("Erro desconhecido em ");
-			break;
-	}
-	printf("(%d,%d): ", position.line, position.column);
-	va_list args;
-	va_start(args, message);
-	vprintf(message, args);
-	va_end(args);
-	printf("\n");
-	if(error == error_fatal)
-		exit(EXIT_FAILURE);
-}
-
-void mark(error_t error, char message[],...)
-{
-	if(error > error_warning)
-		errors_count++;
-	if(errors_count > errors_tolerance)
-	{
-		printf("%d erros foram encontrados. O processo de análise foi cancelado.", errors_tolerance);
-		exit(EXIT_FAILURE);
-	}
-	switch(error)
-	{
-		case error_log:
-			printf("Log");
-			break;
-		case error_info:
-			printf("Info");
-			break;
-		case error_tip:
-			printf("Tip");
-			break;
-		case error_warning:
-			printf("Atenção em ");
-			break;
-		case error_parser:
-			printf("Erro em ");
-			break;
-		case error_fatal:
-			printf("Erro fatal em ");
-			break;
-		default:
-			printf("Erro desconhecido em ");
-			break;
-	}
-	printf("(%d,%d): ", current_token.position.line, current_token.position.column);
-	va_list args;
-	va_start(args, message);
-	vprintf(message, args);
-	va_end(args);
-	printf("\n");
-	if(error == error_fatal)
-		exit(EXIT_FAILURE);
-}
+// Funciona igual a função mark, com exeção que é passada a posição do erro em:
+// position_t positon.
+/*-----------------------------------------------------------------------------------------*/
+void mark_at(error_t error, position_t position, char message[],...);
